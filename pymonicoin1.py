a8c490c7054e9f4939e7ffbe6d3cdd5c22510ce7
Walderlan Sena <senawalderlan@gmail.com>
2017-12-20 23:11:32 -0300

    Adicionado sistema de geração de log dos resultados. E adicionado novas imagens de orientação

--- pymonicoin1.py
+++ pymonicoin1.py
@@ -1,6 +1,8 @@
-#   Monitore o valor do Bitcoin nas Exchanges
+#   Monitore o valor do Bitcoin nas Exchanges/ Monitor the value of Bitcoin
 #   developer: Walderlan Sena
 #   code: https://github.com/WalderlanSena/pymonicoin
+#   version: v1.1.0
+#   license: MIT license
 
 # Importando modulos 
 import urllib.request
@@ -12,13 +14,25 @@ from datetime import datetime
 EXCHANGE  = "FOX"
 BTC_PRICE = "last"
 
+# Funcao que retorna a hora atual do sistema
+def returnHour():
+    horaAtual = datetime.now()
+
+    hora      = horaAtual.hour
+    minuto    = horaAtual.minute
+    segundos  = horaAtual.second
+
+    # Formatando impressao do hora atual
+    horaPrint = "[ " + str(hora) + ":" + str(minuto) + ":" + str(segundos) + " ]"
+    return horaPrint
+
 # Valor do bitcoin depositado em real
 bitcoinDepositado = float('254.0')
 # Valor do bitcoin no momento do deposito anterior
 bitcoinInit       = float('68.998')
 
 # Função que realiza a requisição
-def search():
+def request():
     # Realiza a requisicao para a api que retorna o json com o valor do bitcoin atualmente
     with urllib.request.urlopen("https://api.bitvalor.com/v1/ticker.json") as url:
         # Atribuindo a variavel data o decode da requisicao
@@ -29,28 +43,51 @@ def search():
         bitCalcTemp    = float(bitcoinCurrent*bitcoinDepositado)
         # Recebe valor final da divisao do resultado da multiplicacao * o valor do bitcoin na hora do deposito
         bitcoinFinal   = str(float(bitCalcTemp/bitcoinInit))
-        print("\033[7;42mVALOR DO BITCOIN >>>\033[0m \033[1;42m ฿: ", bitcoinCurrent,"\033[0m\t", "\033[7;42mGANHO EM REAL >>>\033[0m \033[1;42m R$: ", bitcoinFinal[0:6],"\033[0m")
+        
+        # Escrevendo no arquivo de log
+        with open('bit.log', 'a') as arq:
+            # Escrevendo arquivo
+            arq.write("VALOR DO BITCOIN >>> " + str(bitcoinCurrent)  + "\tGANHO EM REAL >>> " + bitcoinFinal[0:6] + " " + returnHour() + "\n")
+            # Fechando o arquivo cujo contem as informacoes de log
+            arq.close()
+        
+        # Imprimindo na tela os valores obtido
+        print("\033[7;42mVALOR DO BITCOIN >>>\033[0m \033[1;42m ฿: ", bitcoinCurrent,"\033[0m\t", "\033[7;42mGANHO EM REAL >>>\033[0m \033[1;42m R$: ", bitcoinFinal[0:6],"\033[0m", returnHour())
 
-# Tela de Splash
-print('''
-        \033[0;0;33m                                  (_)         (_)      
-         _ __  _   _ _ __ ___   ___  _ __  _  ___ ___  _ _ __  
-        | '_ \| | | | '_ ` _ \ / _ \| '_ \| |/ __/ _ \| | '_ \ 
-        | |_) | |_| | | | | | | (_) | | | | | (_| (_) | | | | |
-        | .__/ \__, |_| |_| |_|\___/|_| |_|_|\___\___/|_|_| |_|
-        | |     __/ |                                    v1.0.0      
-        |_|    |___/
-        \033[0m
-                >>> Monitor the value of Bitcoin <<< 
-                    Developer: Walderlan Sena
-            https://github.com/WalderlanSena/pymonicoin
-    ''')
-# Realiza uma requisição incial
-search()
-# Laço infinito
-while True:
-    # Esperando 200 segundos para fazer uma requisição
-    time.sleep(200)
-    # Chamando a funcao que faz a requisicao
-    search()
-# end while
+def splash():
+    # Tela de Splash
+    print('''
+            \033[0;0;33m                                  (_)         (_)      
+             _ __  _   _ _ __ ___   ___  _ __  _  ___ ___  _ _ __  
+            | '_ \| | | | '_ ` _ \ / _ \| '_ \| |/ __/ _ \| | '_ \ 
+            | |_) | |_| | | | | | | (_) | | | | | (_| (_) | | | | |
+            | .__/ \__, |_| |_| |_|\___/|_| |_|_|\___\___/|_|_| |_|
+            | |     __/ |                                    v1.1.0      
+            |_|    |___/
+            \033[0m
+                    >>> Monitor the value of Bitcoin <<< 
+                        Developer: Walderlan Sena
+                https://github.com/WalderlanSena/pymonicoin
+        ''')
+
+def main():
+    # Chamando tela de splash
+    splash()
+    # Realiza uma requisição incial
+    request()
+    # Laço infinito
+    while True:
+        # Esperando 200 segundos para fazer uma requisicao
+        time.sleep(200)
+        # Chamando a funcao que faz a requisicao
+        request()
+    # end while
+
+# Init software
+if __name__ == "__main__":
+    try:
+        # Chamando a funcao main
+        main()
+    except (KeyboardInterrupt):
+        # Mostra o texto posterior caso o usuario interrompa o software
+        print("\n by...")
